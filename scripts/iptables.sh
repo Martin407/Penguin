227f58e3d024191dc818ea27d7cbaaa597c2b4b6
Martin Roberts <temples_spyware0c@icloud.com>
2024-08-06 09:20:59 -0700

    Updating iptables rules

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-# Function to prompt for yes/no
-prompt_yes_no() {
-    while true; do
-        read -p "$1 (y/n): " yn
-        case $yn in
-            [Yy]* ) return 0;;
-            [Nn]* ) return 1;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-}
-
 # Flush existing iptables rules
+sudo iptables-save > /etc/iptables-rules.old
 sudo iptables -F
 sudo iptables -X
 
-# Allow loopback traffic
-if prompt_yes_no "Do you want to allow loopback traffic (in/out)?"; then
-    sudo iptables -A INPUT -i lo -j ACCEPT
-    sudo iptables -A OUTPUT -o lo -j ACCEPT
-fi
+
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A OUTPUT -o lo -j ACCEPT
+
 
 # Allow ICMP traffic
-if prompt_yes_no "Do you want to allow ICMP traffic (in/out)?"; then
-    sudo iptables -A INPUT -p icmp -j ACCEPT
-    sudo iptables -A OUTPUT -p icmp -j ACCEPT
-fi
+sudo iptables -A INPUT -p icmp -j ACCEPT
+sudo iptables -A OUTPUT -p icmp -j ACCEPT
+
+# Allow SMTP traffic outbound
+sudo iptables -A OUTPUT -p tcp --dport 25 -j ACCEPT
+
 
 # Allow DNS traffic outbound
 sudo iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
@@ -42,6 +32,7 @@ IFS=',' read -ra PORT_ARRAY <<< "$ports"
 for port in "${PORT_ARRAY[@]}"; do
     if [[ "$port" =~ ^[0-9]+$ ]]; then
         sudo iptables -A INPUT -p tcp --dport "$port" -j ACCEPT
+        sudo iptables -A OUTPUT -p tcp --sport "$port" -j ACCEPT
     else
         echo "Invalid port: $port. Skipping..."
     fi
@@ -52,7 +43,7 @@ sudo iptables -A INPUT -j LOG
 
 # Save the iptables rules
 if command -v iptables-save &> /dev/null; then
-    sudo iptables-save > /etc/iptables/rules.v4
+    sudo iptables-save > /etc/iptables-rules
 else
     echo "iptables-save command not found. Please ensure iptables-persistent is installed to save rules."
 fi
