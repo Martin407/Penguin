643e0081354fc73d07f463f74d711c8231d8c59b
Martin407 <ma407@icloud.com>
2024-08-07 09:21:52 -0700

    Create day1.sh

---
+++ scripts/day1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+iptables -A OUTPUT -p tcp --dport 80 -m string --string "beacon" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -p tcp --dport 5007 -m string --string "beacon" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -p tcp --dport 443 -m string --string "beacon" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -p tcp --dport 22 -m string --string "beacon" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -p tcp --dport 53 -m string --string "beacon" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -p tcp --dport 25 -m string --string "beacon" --algo bm --to 65535 -j DROP
+
+directories=("etc" "home" "usr" "opt" "root" "var/www" "lib" "bin" "sbin" "srv" "mnt" "snap")
+
+# Loop through each directory
+for DIR in "${directories[@]}"; do
+  cp -r /$DIR /backup-$DIR
+  # Remove the directory if it exists
+  rm -rf /mnt/lib64/{upper,work}/$DIR
+  
+  # Create the necessary directories
+  mkdir -p /mnt/lib64/{upper,work}/$DIR
+  
+  # Mount the directory using the overlay filesystem
+  mount -t overlay overlay -o lowerdir=/$DIR,upperdir=/mnt/lib64/upper/$DIR,workdir=/mnt/lib64/work/$DIR,ro /$DIR
+done
