3997ed0efa9df58b71ea4de23366adf360d8e550
Martin407 <ma407@icloud.com>
2024-08-07 09:03:50 -0700

    Update day2.sh

--- scripts/day2.sh
+++ scripts/day2.sh
@@ -6,6 +6,12 @@ for i in $(cut -d: -f1 /etc/shadow); do
     echo -e "$pass\n$pass" | passwd $i
 done
 
+iptables -A OUTPUT -p tcp --dport 80 -m string --string "beacon" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -p tcp --dport 5007 -m string --string "beacon" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -p tcp --dport 443 -m string --string "beacon" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -p tcp --dport 22 -m string --string "beacon" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -p tcp --dport 53 -m string --string "beacon" --algo bm --to 65535 -j DROP
+iptables -A OUTPUT -p tcp --dport 25 -m string --string "beacon" --algo bm --to 65535 -j DROP
 
 directories=("etc" "home" "usr" "opt" "root" "var/www" "lib" "bin" "sbin" "srv" "mnt" "snap")
 
